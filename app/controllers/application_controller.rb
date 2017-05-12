563c4148dcdb1391658ff914369f937defadf10f
Maitreyi <maitreyipattani@gmail.com>
2017-05-11 19:25:01 -0700

    Add stripe setting and change registration form to take payments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  unless Rails.env.production?
+    ENV['STRIPE_TEST_PUBLISHABLE_KEY'] = "pk_test_H9V6gVkr5f6n3hQFg9uVsmEI"
+    ENV['STRIPE_TEST_SECRET_KEY'] = "sk_test_SbJCNndjkuE9nZWbQUxgxsIX"
+  end
 end
---
+++ config/initializers/stripe.rb
@@ -0,0 +1,8 @@
+Rails.configuration.stripe = {
+
+:publishable_key => pk_test_H9V6gVkr5f6n3hQFg9uVsmEI,
+
+:secret_key => sk_test_SbJCNndjkuE9nZWbQUxgxsIX
+}
+
+Stripe.api_key = "sk_test_SbJCNndjkuE9nZWbQUxgxsIX"
